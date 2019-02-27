1f6d28ed65984aa69f26415c7f86e093fec0166a
vonhenry <amorofroma@gmail.com>
2019-02-27 15:39:27 +0800

    update

--- test_eosio_token.sh
+++ test_eosio_token.sh
@@ -8,9 +8,17 @@ contract_folder=eosio.token
 keypair_1_prikey=5JfCFmHKKzGeJzEFmnzgwESQDyz4NXYyyMogxSvnYT1JaMYpJmG
 keypair_1_pubkey=EOS6tpJ5WzqWde2nKUQYmfPRVmoUHFmCdtKG25RkQqWEaBFgTavE7
 
+issuer=mytoken11111
+user1=eosiouser111
+user2=eosiouser112
+
 create_some_account(){
     create_account_by_name_pubkey ${contract} ${keypair_1_pubkey}
     import_key ${keypair_1_prikey}
+
+    create_account_by_name ${issuer}
+    create_account_by_name ${user1}
+    create_account_by_name ${user2}
 }
 create_some_account
 
@@ -23,21 +31,32 @@ set_contract
 echo  -------------------- ACTIONS TEST ------------------------
 
 action_create(){
-    cleos push action eosio.token create '["eosio", "10000000000.0000 EOS"]' -p eosio.token
+    cleos push action ${contract} create '["'${contract}'", "1000000.0000 GOLD"]' -p ${contract}
+
+    cleos get table ${contract} GOLD stat
+    cleos get currency balance ${contract} ${contract} GOLD
+    cleos get currency balance eosio.token ${contract} EOS
 }
 
 action_issue(){
-    cleos push action eosio.token issue '["eosio",  "1000000000.0000 EOS", "memo"]' -p eosio
+    cleos push action ${contract} issue '["'${contract}'", "100000.0000 GOLD", "memo"]' -p ${contract}
+
+    cleos get table ${contract} GOLD stat
+    cleos get currency balance ${contract} ${contract} GOLD
 }
 
 action_transfer(){
-    echo
+    cleos get currency balance ${contract} ${user1} GOLD
+    cleos get currency balance ${contract} ${user2} GOLD
+
+    cleos push action ${contract} transfer '["'${contract}'", '${user1}', "100.0000 GOLD", "memo"]' -p ${contract}
+
+    cleos push action ${contract} transfer '["'${user1}'", '${user2}', "10.0000 GOLD", "memo"]' -p ${user1}
 }
 
 
-
 actions_test(){
-    action_hi
+    echo
 }
 actions_test
 
